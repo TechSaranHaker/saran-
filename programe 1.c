d76e4beff2ed98e9d09b2b1945bfb6476f71e9e2
TechSaranHaker <151424886+TechSaranHaker@users.noreply.github.com>
2023-11-19 18:27:26 +0530

    Add files via upload

---
+++ programe 1.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a<100)
+	{
+		printf("a is lessthen hundred");
+	}
+	else if(a==100)
+	{printf("a equal of hundred");
+	}
+	else
+	{printf("saran");
+	}
+	getch();
+}
+
